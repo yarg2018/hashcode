eee110ac642736593b4240db2db711abff0645a8
vroby <vroby@vrobylinux.homenet.telecomitalia.it>
2018-02-23 21:27:56 +0100

    droni

---
+++ pizza2018/example.php
@@ -0,0 +1,21 @@
+#!/usr/bin/php
+<?php
+$example=file("example.in");
+$dati=explode(" ", $example[0]);
+$righe=$dati[0];
+$colonne=$dati[1];
+
+
+echo $righe."\n";
+echo $colonne."\n";
+
+for($x=0;$x<$colonne;$x++){
+	for($x=0;$x<$colonne;$x++){
+		
+	}
+}
+
+
+
+
+?>
