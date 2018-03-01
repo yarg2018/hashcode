875426d1bfb2e51c81c2c7a3404c4298a3059c56
vroby65 <vroby.mail@gmail.com>
2018-03-01 17:27:38 +0100

    gg

--- droni/qualification_round_2016.in/test.php
+++ droni/qualification_round_2016.in/test.php
@@ -10,7 +10,7 @@ $drones=$dati[2];
 $max_turns=$dati[3];
 $max_payload=$dati[4];
 $product_types=$file[1];
-$product_weight=explode(" ", File[2]);
+$product_weight=explode(" ", $file[2]);
 $warehouses=$file[3];
 $n=4;
 for($i=0; $i<$warehouses;$i++){
@@ -30,6 +30,7 @@ for ($w=0;$w<$warehouses;$w++){
 	for($o=0; $o<$n_orders; $o++){
 		$cur_distanza=distanza($wa[$w][0],$wa[$w][1],$orders_delivery[$o][0],$orders_delivery[$o][1]);
 		if ($cur_distanza<$min_distanza){
+		
 			$min_distanza=$cur_distanza;
 			$min_warehouse=$w;
 			$min_orders=$o;
@@ -41,7 +42,7 @@ echo "mindistanza $min_distanza $min_warehouse $min_orders " ."\n";
 
 
 
-
+/*
 echo "row ".$row."\n";
 echo "columns ".$columns."\n";
 echo "drones ".$drones."\n";
@@ -49,7 +50,7 @@ echo "max turns ".$max_turns."\n";
 echo "max payload  ".$max_payload."\n";
 
 echo "warehoses ".$warehouses."\n";
-
+*/
 
 
 ?>
