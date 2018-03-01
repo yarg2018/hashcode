be88251fd9194a0462547e16c7c0367728d6e8a6
xquiet <matteo.pasotti@gmail.com>
2018-03-01 19:39:19 +0100

    distance calculation

--- 2018/test1.php
+++ 2018/test1.php
@@ -1,5 +1,5 @@
 <?php
-#include  "utility";
+require_once("utility.php");
 $file=file("test.in");
 $n=0;
 $tmp=explode( " ",$file[$n++]);
@@ -11,9 +11,13 @@ $rides=$tmp[3];
 $bonus =$tmp[4];
 $steps=$tmp[5];
 
+$corse = array();
+
 for ($i=0; $i< $rides; $i++){
-	$ride = array("a"=>0,"b"=>0,"x"=>0,"y"=>0,"s"=>0,"f"=>0);
+	$ride = array('a'=>0,'b'=>0,'x'=>0,'y'=>0,'s'=>0,'f'=>0,'distance'=>0);
 	list($ride['a'],$ride['b'],$ride['x'],$ride['y'],$ride['s'],$ride['f']) = explode(" ",$file[$n++]);
+	$ride['distance'] = distanza($ride['a'],$ride['b'],$ride['x'],$ride['y']);
+	array_push($corse, $ride);
 }
 
 
