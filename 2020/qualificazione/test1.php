42d03cc8389dc67c22fe4327c1b566ddd318ef39
vroby <vroby.mail@gmail.com>
2020-02-20 19:02:31 +0100

    Add files via upload

---
+++ 2020/qualificazione/test1.php
@@ -0,0 +1,16 @@
+<?php
+
+
+$fname[0]='a_example.txt';
+$fname[0]='b_read_on.txt';	     
+$fname[0]='c_incunabula.txt';     
+$fname[0]='d_tough_choices.txt';  
+$fname[0]='e_so_many_books.txt';
+$fname[0]='f_libraries_of_the_world.txt';
+
+
+
+
+
+
+?>
